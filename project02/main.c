dea41e2232b0496c8a94229c70891cbf9b932c9c
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2023-11-28 02:00:44 -0300

    IO now working

--- project02/main.c
+++ project02/main.c
@@ -327,16 +327,76 @@ void preemptionProcess(int *elapsedQuantum, int quantum, Process** currentCPUPro
 }
 
 /*
+    IO call event
+*/
+void callIO(Process** currentCPUProcess, Queue* disk, Queue* tape, Queue* printer, int *elapsedQuantum) {
+    if ((*currentCPUProcess)->currentIO == (*currentCPUProcess)->numIOs) {
+        return;
+    }
+
+    IO io = (*currentCPUProcess)->ios[(*currentCPUProcess)->currentIO];
+    if ((*currentCPUProcess)->processedTime == io.startTime) {
+        printf("Process %d called %s\n", (*currentCPUProcess)->pid, io.name);
+
+        QueueElement *newElement = (QueueElement *) malloc(sizeof(QueueElement));
+        if (newElement == NULL) {
+            printf("Error creating new queue element\n");
+            exit(1);
+        }
+        newElement->process = *currentCPUProcess;
+        newElement->next = NULL;
+
+        switch (io.duration) {
+            case DISK:
+                if (disk->first == NULL) {
+                    disk->first = newElement;
+                    disk->last = newElement;
+                } else {
+                    disk->last->next = newElement;
+                    disk->last = newElement;
+                }
+                break;
+            case TAPE:
+                if (tape->first == NULL) {
+                    tape->first = newElement;
+                    tape->last = newElement;
+                } else {
+                    tape->last->next = newElement;
+                    tape->last = newElement;
+                }
+                break;
+            case PRINTER:
+                if (printer->first == NULL) {
+                    printer->first = newElement;
+                    printer->last = newElement;
+                } else {
+                    printer->last->next = newElement;
+                    printer->last = newElement;
+                }
+                break;
+            default:
+                printf("Error calling IO\n");
+                exit(1);
+                break;
+        }
+
+        (*currentCPUProcess)->state = BLOCKED;
+        (*currentCPUProcess) = NULL;
+        *elapsedQuantum = 0;
+    }
+}
+
+/*
     Advance the current CPU process event
 */
-void advanceCPUProcess(Process** currentCPUProcess, Queue *highPriority, Queue *lowPriority, int *terminatedProcesses, int quantum, int *elapsedQuantum) {
+void advanceCPUProcess(Process** currentCPUProcess, Queues *queues, int *terminatedProcesses, int quantum, int *elapsedQuantum) {
     if (*currentCPUProcess == NULL) {
-        if (highPriority->first != NULL) {
-            *currentCPUProcess = highPriority->first->process;
-            highPriority->first = highPriority->first->next;
-        } else if (lowPriority->first != NULL) {
-            *currentCPUProcess = lowPriority->first->process;
-            lowPriority->first = lowPriority->first->next;
+        if (queues->highPriority->first != NULL) {
+            *currentCPUProcess = queues->highPriority->first->process;
+            queues->highPriority->first = queues->highPriority->first->next;
+        } else if (queues->lowPriority->first != NULL) {
+            *currentCPUProcess = queues->lowPriority->first->process;
+            queues->lowPriority->first = queues->lowPriority->first->next;
         } else {
             printf("No process to run\n");
             return;
@@ -351,8 +411,102 @@ void advanceCPUProcess(Process** currentCPUProcess, Queue *highPriority, Queue *
 
     terminateProcess(currentCPUProcess, terminatedProcesses, elapsedQuantum);
 
-    if (*currentCPUProcess != NULL) {
-        preemptionProcess(elapsedQuantum, quantum, currentCPUProcess, highPriority, lowPriority);
+    if (*currentCPUProcess != NULL) callIO(currentCPUProcess, queues->disk, queues->tape, queues->printer, elapsedQuantum);
+    if (*currentCPUProcess != NULL) preemptionProcess(elapsedQuantum, quantum, currentCPUProcess, queues->highPriority, queues->lowPriority);
+}
+
+/*
+    Advance the current IO processes event
+*/
+void advanceIOProcess(Queues *queues) {
+    // Disk queue
+    if (queues->disk->first != NULL) {
+        queues->disk->first->process->elapsedIOTime++;
+        if (queues->disk->first->process->elapsedIOTime == DISK) {
+            printf("Process %d finished disk IO\n", queues->disk->first->process->pid);
+            queues->disk->first->process->currentIO++;
+            queues->disk->first->process->state = READY;
+            queues->disk->first->process->priority = LOW;
+            queues->disk->first->process->elapsedIOTime = 0;
+
+            QueueElement *newElement = (QueueElement *) malloc(sizeof(QueueElement));
+            if (newElement == NULL) {
+                printf("Error creating new queue element\n");
+                exit(1);
+            }
+            newElement->process = queues->disk->first->process;
+            newElement->next = NULL;
+
+            if (queues->lowPriority->first == NULL) {
+                queues->lowPriority->first = newElement;
+                queues->lowPriority->last = newElement;
+            } else {
+                queues->lowPriority->last->next = newElement;
+                queues->lowPriority->last = newElement;
+            }
+
+            queues->disk->first = queues->disk->first->next;
+        }
+    }
+
+    // Tape queue
+    if (queues->tape->first != NULL) {
+        queues->tape->first->process->elapsedIOTime++;
+        if (queues->tape->first->process->elapsedIOTime == TAPE) {
+            printf("Process %d finished tape IO\n", queues->tape->first->process->pid);
+            queues->tape->first->process->currentIO++;
+            queues->tape->first->process->state = READY;
+            queues->tape->first->process->priority = HIGH;
+            queues->tape->first->process->elapsedIOTime = 0;
+
+            QueueElement *newElement = (QueueElement *) malloc(sizeof(QueueElement));
+            if (newElement == NULL) {
+                printf("Error creating new queue element\n");
+                exit(1);
+            }
+            newElement->process = queues->tape->first->process;
+            newElement->next = NULL;
+
+            if (queues->highPriority->first == NULL) {
+                queues->highPriority->first = newElement;
+                queues->highPriority->last = newElement;
+            } else {
+                queues->highPriority->last->next = newElement;
+                queues->highPriority->last = newElement;
+            }
+
+            queues->tape->first = queues->tape->first->next;
+        }
+    }
+
+    // Printer queue
+    if (queues->printer->first != NULL) {
+        queues->printer->first->process->elapsedIOTime++;
+        if (queues->printer->first->process->elapsedIOTime == PRINTER) {
+            printf("Process %d finished printer IO\n", queues->printer->first->process->pid);
+            queues->printer->first->process->currentIO++;
+            queues->printer->first->process->state = READY;
+            queues->printer->first->process->priority = HIGH;
+            queues->printer->first->process->elapsedIOTime = 0;
+
+            QueueElement *newElement = (QueueElement *) malloc(sizeof(QueueElement));
+            if (newElement == NULL) {
+                printf("Error creating new queue element\n");
+                exit(1);
+            }
+            newElement->process = queues->printer->first->process;
+            newElement->next = NULL;
+
+            if (queues->highPriority->first == NULL) {
+                queues->highPriority->first = newElement;
+                queues->highPriority->last = newElement;
+            } else {
+                queues->highPriority->last->next = newElement;
+                queues->highPriority->last = newElement;
+            }
+
+            queues->printer->first = queues->printer->first->next;
+        }
     }
 }
 
@@ -365,18 +519,40 @@ void simulate(Scheduler *scheduler) {
         printf("\n=====Current time: %d=====\n", currentTime);
         
         enterNewProcess(scheduler->processes, scheduler->numProcesses, scheduler->queues, currentTime);
-        advanceCPUProcess(&(scheduler->currentCPUProcess), scheduler->queues->highPriority, scheduler->queues->lowPriority, &(scheduler->terminatedProcesses), scheduler->quantum, &(scheduler->elapsedQuantum));
-        //print the head of high and low priority queues
-        if (scheduler->queues->highPriority->first != NULL) {
-            printf("High priority queue: %d\n", scheduler->queues->highPriority->first->process->pid);
-        } else {
-            printf("High priority queue: empty\n");
+        advanceCPUProcess(&(scheduler->currentCPUProcess), scheduler->queues, &(scheduler->terminatedProcesses), scheduler->quantum, &(scheduler->elapsedQuantum));
+        advanceIOProcess(scheduler->queues);
+        // print head of all queues
+        printf("High priority queue: ");
+        QueueElement *ptr = scheduler->queues->highPriority->first;
+        while (ptr != NULL) {
+            printf("%d ", ptr->process->pid);
+            ptr = ptr->next;
         }
-        if (scheduler->queues->lowPriority->first != NULL) {
-            printf("Low priority queue: %d\n", scheduler->queues->lowPriority->first->process->pid);
-        } else {
-            printf("Low priority queue: empty\n");
+        printf("\nLow priority queue: ");
+        ptr = scheduler->queues->lowPriority->first;
+        while (ptr != NULL) {
+            printf("%d ", ptr->process->pid);
+            ptr = ptr->next;
         }
+        printf("\nDisk queue: ");
+        ptr = scheduler->queues->disk->first;
+        while (ptr != NULL) {
+            printf("%d ", ptr->process->pid);
+            ptr = ptr->next;
+        }
+        printf("\nTape queue: ");
+        ptr = scheduler->queues->tape->first;
+        while (ptr != NULL) {
+            printf("%d ", ptr->process->pid);
+            ptr = ptr->next;
+        }
+        printf("\nPrinter queue: ");
+        ptr = scheduler->queues->printer->first;
+        while (ptr != NULL) {
+            printf("%d ", ptr->process->pid);
+            ptr = ptr->next;
+        }
+        printf("\n");
 
         currentTime++;
     }
